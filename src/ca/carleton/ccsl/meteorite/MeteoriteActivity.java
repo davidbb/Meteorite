f3854051756d451a7a82efe397c78b611850bd0f
Daniel McCarney <daniel@binaryparadox.net>
2012-05-19 02:11:30 -0400

    Some general clean-up & code reduction.
    AndroidManifest.xml:
      Normalized indentation/whitespace for readability.
    
    strings.xml:
      Added an apostrophe to the "hello" text.
    
    PackageReceiver.java:
      Added javadoc strings.
      Removed unused code.
      Changed TAG to use classname.
      Created static final constants for package prefix & toast duration.
    
    UAppIDUtils.java:
      Normalized indentation/whitespace for readability.
      Reduced toHexString code.

--- src/ca/carleton/ccsl/meteorite/MeteoriteActivity.java
+++ src/ca/carleton/ccsl/meteorite/MeteoriteActivity.java
@@ -3,10 +3,12 @@ package ca.carleton.ccsl.meteorite;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MeteoriteActivity extends Activity {
+public class MeteoriteActivity extends Activity 
+{
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
